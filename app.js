8a3a992a058f1935b5af845b261b21cbde534a87
Jordan EC2 <jnielson94@gmail.com>
2017-10-14 18:35:02 +0000

    Giving up on restricting HTML pages

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 var styles = express.Router().use(express.static(path.join(__dirname,'public','stylesheets')))
 app.use('/stylesheets', styles);
 app.use(cors());
