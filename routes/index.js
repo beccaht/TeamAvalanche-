bffdbfdc396891a0bc94081b0d605e1a25f4ce2c
jnielson94 <jnielson94@gmail.com>
2017-10-14 15:30:49 -0600

    Working on routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user')
+var Employee = require('../models/employee')
+var Company = require('../models/company');
+var Project = require('../models/project');
+
 /* GET home/login page. */
 router.get('/', function(req, res, next) {
   res.sendFile('index.html', {'root': 'public'});
@@ -10,5 +15,27 @@ router.get('/', function(req, res, next) {
 router.get('/signUp.html', function(req, res, next) {
   res.sendFile('signUp.html', {'root': 'public'});
 });
+
+router.get('/projects', function(req,res,next) {
+  Project.find().then(projects => {
+    return res.json(projects);
+  })
+  })
+  
+  router.get('/companies', function(req,res,next) {
+    Company.find().then(companies => {
+      return res.json(companies);
+    })
+  })
+  
+  router.get('/company', function(req,res,next) {
+    if(req.body.companyName) {
+      Company.find({"name":req.body.companyName}).then(companies => {
+        return res.json(companies);
+      })
+
+    }
+  })
+
 
 module.exports = router;
