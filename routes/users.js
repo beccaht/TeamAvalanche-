63b2ff9ea18930240b899899f0791b8d8edcffd3
jnielson94 <jnielson94@gmail.com>
2017-10-14 14:11:57 -0600

    Trying to send data back

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,14 @@ var model = mongoose.model('bruteforce', BruteForceSchema)
 var store = new MongooseStore(model);
 
 var bruteforce = new ExpressBrute(store, {
-	freeRetries: 5,
-	minWait: 5*60*1000, // 5 minutes
-	maxWait: 60*60*1000, // 1 hour,
+  freeRetries: 5,
+  minWait: 5 * 60 * 1000, // 5 minutes
+  maxWait: 60 * 60 * 1000, // 1 hour,
 });
 
 //Our User model
 var User = require('../models/user')
+var Employee = require('../models/employee');
 
 //Add this as a middleware if you need a route with login
 function requiresLogin(req, res, next) {
@@ -32,59 +33,84 @@ function requiresLogin(req, res, next) {
 }
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Hi!');
 });
 
-router.post('/signup', bruteforce.prevent, function(req,res,next) {
+router.post('/signup', bruteforce.prevent, function (req, res, next) {
   console.log("Signing up a user!", req.body);
-    User.find().lean().then(function(users) {
-      console.log(users);
-      if(users.length === 0) {
-        req.body.roles = ["admin","employee","employerAdmin"];
-      }
-      console.log(req.body);
-      if (req.body.email &&
-        req.body.password &&
-        req.body.roles) {
-        var userData = {
-      email: req.body.email,
-      password: req.body.password,
-      roles: req.body.roles,
-      firstName: req.body.firstName || "",
-      lastName: req.body.lastName || "",
-      avatarUrl: req.body.avatarUrl || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+  User.find().lean().then(function (users) {
+    console.log(users);
+    if (users.length === 0) {
+      req.body.roles = ["admin", "employee", "employerAdmin"];
     }
-    console.log(userData);
-    //use schema.create to insert data into the db
-    User.create(userData, function (err, user) {
-      if (err) {
-        return next(err)
-      } else {
-        req.session.userId = user._id;
-        req.session.roles = user.roles;
-        if(user.roles.length > 1) {
-          return res.redirect('/users/roleChoice');
-        }
-        else if(user.roles.includes("admin")) {
-          return res.redirect('/admin/');
-        }
-        else if(user.roles.includes("employerAdmin")) {
-          return res.redirect('/employers/');
-        }
-        else {
-          return res.redirect('/employees/');
-        }
+    else {
+      req.body.roles = ["employee"];
+    }
+    console.log(req.body);
+    if (req.body.email &&
+      req.body.password &&
+      req.body.roles) {
+      var userData = {
+        email: req.body.email,
+        password: req.body.password,
+        roles: req.body.roles,
+        firstName: req.body.firstName || "",
+        lastName: req.body.lastName || "",
+        avatarUrl: req.body.avatarUrl || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
       }
-    });
-  }
-  else {
-  console.log("Missing something!", req.body);
-  }
-    })
+      if (req.body.companyCode) {
+        Employee.create({ email: req.body.email, company: req.body.companyCode }, function (err, employee) {
+          if (err) {
+            return next(err)
+          }
+          else {
+            req.session.employee = employee;
+          }
+        })
+      }
+      console.log(userData);
+      //use schema.create to insert data into the db
+      User.create(userData, function (err, user) {
+        if (err) {
+          return next(err)
+        } else {
+          req.session.userId = user._id;
+          req.session.roles = user.roles;
+          if (user.roles.length > 1) {
+            return res.json({
+              "data": '/users/roleChoice',
+              "info": req.session
+            });
+          }
+          else if (user.roles.includes("admin")) {
+            return res.json({
+              "data": '/admin',
+              "info": req.session
+            });
+          }
+          else if (user.roles.includes("employerAdmin")) {
+            return res.json({
+              "data": '/employers',
+              "info": req.session
+            });
+          }
+          else {
+            return res.json({
+              "data": '/employees',
+              "info": req.session
+            });
+          }
+        }
+      });
+    }
+    else {
+      console.log("Missing something!", req.body);
+    }
+  })
 })
 
-router.post('/login', bruteforce.prevent, function(req,res,next) {
+router.post('/login', bruteforce.prevent, function (req, res, next) {
   console.log("Logging in a User!");
   User.authenticate(req.body.email, req.body.password, function (error, user) {
     if (error || !user) {
@@ -94,18 +120,61 @@ router.post('/login', bruteforce.prevent, function(req,res,next) {
     } else {
       req.session.userId = user._id;
       req.session.roles = user.roles;
-      if(user.roles.length > 1) {
-        return res.json({"data":'/users/roleChoice'});
-      }
-      else if(user.roles.includes("admin")) {
-        return res.json({"data":'/admin'});
-      }
-      else if(user.roles.includes("employerAdmin")) {
-        return res.json({"data":'/employers/'});
+      if(req.session.roles.includes("employee")) {
+        Employee.find().then(employee => {
+          req.session.employee = employee;
+          if (user.roles.length > 1) {
+            return res.json({
+              "data": '/users/roleChoice',
+              "info": req.session
+            });
+          }
+          else if (user.roles.includes("admin")) {
+            return res.json({
+              "data": '/admin',
+              "info": req.session
+            });
+          }
+          else if (user.roles.includes("employerAdmin")) {
+            return res.json({
+              "data": '/employers',
+              "info": req.session
+            });
+          }
+          else {
+            return res.json({
+              "data": '/employees',
+              "info": req.session
+            });
+          }  
+        })
       }
       else {
-        return res.json({"data":'/employees/'});
-      }    
+        if (user.roles.length > 1) {
+          return res.json({
+            "data": '/users/roleChoice',
+            "info": req.session
+          });
+        }
+        else if (user.roles.includes("admin")) {
+          return res.json({
+            "data": '/admin',
+            "info": req.session
+          });
+        }
+        else if (user.roles.includes("employerAdmin")) {
+          return res.json({
+            "data": '/employers',
+            "info": req.session
+          });
+        }
+        else {
+          return res.json({
+            "data": '/employees',
+            "info": req.session
+          });
+        }
+      }
     }
   });
 })
