e9f98619ec26cc1d6a082f118054c5f1e2b860d5
jnielson94 <jnielson94@gmail.com>
2017-10-14 12:48:45 -0600

    Trying out routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
+//Add this as a middleware if you need a route with login
+function requireLogin(req, res, next) {
+    console.log("This route requries login!");
+    if (req.session && req.session.userId) {
+            return next();
+    } else {
+      var err = new Error('You must be authorized to view this page.');
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+router.use('/', requireLogin);
 /* GET home/login page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html', {'root': 'public'});
+  res.sendFile('menu.html', {'root': '../public/admin'});
 });
 
 /* GET signup page */
